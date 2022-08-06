a7691b959187d09cd71f3f7c1305fea79d2cebdf
Thiệu Trung Kiên <87187870+TrunqKj3n@users.noreply.github.com>
2022-08-06 19:51:24 +0700

    Update soundcloud.py

--- soundcloud.py
+++ soundcloud.py
@@ -1,39 +1,41 @@
 from tracemalloc import reset_peak
 import requests
-import json
 import pygame
 from colorama import Fore
 from colorama import Style
-url = input("Vui lòng nhập URL cần tải: ")
-link = "https://www.klickaud.co/download.php"
-res = requests.post(link, data={
-    "value": url
-})
-getUrl = res.text.split('onclick="downloadFile(')[1].split(';')[
-    0].split(',')[0].replace("'", "")
-# save audio
-res1 = requests.get(getUrl)
-with open("audio.mp3", "wb") as f:
-    f.write(res1.content)
-print(Fore.GREEN + "Tải xuống thành công!")
-print(Style.RESET_ALL)
-reset_peak()
-# play audio
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load("audio.mp3")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
-pygame.mixer.music.stop()
-pygame.quit()
-pygame.mixer.quit()
-pygame.display.quit()
-pygame.quit()
-print(Fore.GREEN + "Xong!")
-print(Style.RESET_ALL)
+def soundcloud(url):
+    link = "https://www.klickaud.co/download.php"
+    res = requests.post(link, data={
+        "value": url
+    })
+    getUrl = res.text.split('onclick="downloadFile(')[1].split(';')[
+        0].split(',')[0].replace("'", "")
+    # save audio
+    res1 = requests.get(getUrl)
+    with open("audio.mp3", "wb") as f:
+        f.write(res1.content)
+    print(Fore.GREEN + "Tải xuống thành công!")
+    print(Style.RESET_ALL)
+    reset_peak()
+    # play audio
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load("audio.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    pygame.mixer.music.stop()
+    pygame.quit()
+    pygame.mixer.quit()
+    pygame.display.quit()
+    pygame.quit()
+    print(Fore.GREEN + "Xong!")
+    print(Style.RESET_ALL)
 # delete audio
-import os
-os.remove("audio.mp3")
-print(Fore.GREEN + "Xóa thành công!")
-print(Style.RESET_ALL)
+    import os
+    os.remove("audio.mp3")
+    print(Fore.GREEN + "Xóa thành công!")
+    print(Style.RESET_ALL)
+
+url = input(f"{Fore.LIGHTBLUE_EX}Nhập link nhạc soundcloud: ")
+soundcloud(url)
